47e09ce594cf6832f183797993a46867546b7129
lutakite <lutakite@gmail.com>
2020-10-20 16:50:54 +0300

    fix commit

--- src/app/core/pipes/PennyPipe.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'penny' })
-export class PennyPipe implements PipeTransform {
-    transform(price: number): string {
-        return price.toString().split('.')[1];
-    }
-}
--- src/app/core/pipes/RublePipe.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'ruble' })
-export class RublePipe implements PipeTransform {
-    transform(price: number): string {
-        return price.toString().split('.')[0];
-    }
-}
